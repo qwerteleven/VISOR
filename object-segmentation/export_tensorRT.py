d4cb5a2e1fe8eaf290b35186640db24b6c838781
qwerteleven <ll11ll1@outlook.es>
2026-06-05 09:43:47 +0000

    add logs to tensorRT export

--- object-segmentation/export_tensorRT.py
+++ object-segmentation/export_tensorRT.py
@@ -1,15 +1,16 @@
 
-import json
-
-with open("config.json") as f:
-    config = json.load(f)
-
-config = config["export_tensorRT"]
-
-
 import os
 os.environ['CUDA_MODULE_LOADING'] = 'LAZY'
 import tensorrt as trt
+import sys
+import logging
+
+root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_folder)
+
+from utils.io import get_config, set_logger
+config = get_config("config.json", "export_tensorRT") 
+set_logger("logs", os.path.basename(sys.argv[0]))
 
 
 logger = trt.Logger(trt.Logger.WARNING)
@@ -17,29 +18,30 @@ trt.init_libnvinfer_plugins(logger, "")
 model_path = config["model_path"] 
 
 
+
 class MyLogger(trt.ILogger):
     def __init__(self):
        trt.ILogger.__init__(self)
 
     def log(self, severity, msg):
+        logging.info(msg)
         print(severity, msg)
-        pass # Your custom logging implementation here
 
 logger = MyLogger()
 
 builder = trt.Builder(logger)
-
 network = builder.create_network()
-
 parser = trt.OnnxParser(network, logger)
 
 success = parser.parse_from_file(model_path)
 
 for idx in range(parser.num_errors):
     print(parser.get_error(idx))
+    logging.error(parser.get_error(idx))
 
 if not success:
     print(parser.get_error(idx))
+    logging.error(parser.get_error(idx))
 
 
 config_build = builder.create_builder_config()
@@ -52,14 +54,16 @@ if builder.platform_has_fast_fp16:
 try:
     config_build.builder_optimization_level = 5      # 0–5; 5 = longest build, fastest engine
 except AttributeError:
-    pass  # older TRT version
-
+    logging.error("older TRT version")
+    print("older TRT version")
 
 serialized_engine = builder.build_serialized_network(network, config_build)
 
 
-with open(config["output_name"], "wb") as f:
-    f.write(serialized_engine)
-
-
+try: 
+    with open(config["output_name"], "wb") as f:
+        f.write(serialized_engine)
+except:
+    logging.error("can not write the engine, check disk space, and logs")
+    print("can not write the engine, check disk space, and logs")
 
