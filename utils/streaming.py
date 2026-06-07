892abaa2695a917304b514ba7a8f7a5353ba8a16
qwerteleven <ll11ll1@outlook.es>
2026-06-07 22:16:19 +0000

    add streaming with vidgear, to input output to rtsp-server

--- utils/streaming.py
+++ utils/streaming.py
@@ -3,6 +3,10 @@ from typing import Dict
 import logging
 import traceback
 
+from vidgear.gears import CamGear
+from vidgear.gears import WriteGear
+
+
 from utils.user_event import click_and_crop
 import utils.globals as globals
 
@@ -64,3 +68,72 @@ def streaming_pipeline_OpenCV(config: Dict, ml_model: object, cam: object = None
 
     cv2.destroyAllWindows()
     cam.release()
+
+
+
+def streaming_pipeline_vidgear(config: Dict, ml_model: object, input_source: str, input_config: Dict, output_source: str, output_config: Dict, custom_ffmpeg: str = "../rtsp-server/ffmpeg.exe") -> None:
+    """
+    
+        Takes a input of streaming, executes the ml model over the frames and stremaing the result to rtsp-server
+
+    Args:
+        config (Dict): config of the interface and user inputs
+        ml_model (object): ml model tha inference overr the frames
+        input_source (str): url to streaming, can be a disk path
+        input_config (Dict): config for the FFMPEG, by default use h264_cuid
+        output_source (str): url to streaming the procesed frames, can be a disk path
+        output_config (Dict): config for the FFMPEG, by default use h264_nvenc
+        custom_ffmpeg (str, optional): path to the executable of FFMPEG. Defaults to "../rtsp-server/ffmpeg.exe".
+
+    Raises:
+        Exception: check if can read frames from input source
+        Exception: check if can do the inference over the frame
+    """    
+    stream = CamGear(
+        source = input_source, 
+        **input_config, 
+        custom_ffmpeg = custom_ffmpeg
+    ).start() 
+
+    streamer = WriteGear(
+        output = output_source, 
+        **output_config, 
+        custom_ffmpeg = custom_ffmpeg
+    )  
+
+
+    globals.init()
+    # change it by webRTC inputs of user cv2.setMouseCallback(config["VISOR_NAME"], click_and_crop)
+
+    for _ in range(config["max_iteration"]):
+        image = stream.read()
+
+        if image is None:
+            msg = "failed to grab frame"
+            logging.error(msg)
+            print(msg)
+            raise Exception(msg)
+
+        try:
+            ml_model.update_attention_region(globals.user_ref_point, image.shape)
+            output_image = ml_model(image)
+        except Exception as e:
+            msg = "can not make inference with ml_model"
+            logging.error(msg)
+            print(msg)
+            print(traceback.format_exc())
+            raise Exception(msg)
+        
+        try:
+            streamer.write(output_image)
+        except Exception as e:
+            msg = f"can not write to the output_source: {output_source}"
+            print(msg)
+            logging.error(msg)
+            print(traceback.format_exc())
+            raise Exception(msg)
+
+
+    stream.stop()
+    streamer.close()
+
